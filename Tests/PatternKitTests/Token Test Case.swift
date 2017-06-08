43703720bb1593b978fe29603e7e668a539f4fb9
Constantino Tsarouhas <ctxppc@gmail.com>
2017-06-09 00:26:37 +0200

    Added test cases for Token.

---
+++ Tests/PatternKitTests/Token Test Case.swift
@@ -0,0 +1,41 @@
+// PatternKit © 2017 Constantino Tsarouhas
+
+import XCTest
+@testable import PatternKit
+
+class TokenTestCase : XCTestCase {
+	
+	func testLiteralToken() {
+		
+		let prefix = Literal([1])
+		let token = Token(Literal([2, 3]))
+		let suffix = Literal([4])
+		
+		let pattern = prefix • token • suffix
+		let matches = pattern.matches(over: [1, 2, 3, 4])
+		
+		guard let match = matches.first, matches.count == 1 else { return XCTFail() }
+		
+		let captures = match.capturedSubsequences(for: token)
+		guard let capture = captures.first, captures.count == 1 else { return XCTFail() }
+		
+		XCTAssert(capture == [2, 3])
+		
+	}
+	
+	func testCharacterSetToken() {
+		
+		let token = Token(("a"..."b") • ("a"..."b"))
+		let pattern = literal("a") • token • literal("a")
+		let matches = pattern.matches(over: "abba")
+		
+		guard let match = matches.first, matches.count == 1 else { return XCTFail() }
+		
+		let captures = match.capturedSubsequences(for: token)
+		guard let capture = captures.first, captures.count == 1 else { return XCTFail() }
+		
+		XCTAssert(String(capture) == "bb")
+		
+	}
+	
+}
