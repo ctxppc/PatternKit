9caea91853fae167e9c179de0abe7fbae5dbc341
Constantino Tsarouhas <ctxppc@gmail.com>
2017-06-20 14:13:01 +0200

    Added test cases for Negated Forward/Backward Assertion.

--- Tests/PatternKitTests/Negated Backward Assertions Test Case.swift
+++ Tests/PatternKitTests/Negated Backward Assertions Test Case.swift
@@ -5,4 +5,29 @@ import XCTest
 
 class NegatedBackwardAssertionsTestCase : XCTestCase {
 	
+	func testSimpleAssertion() {
+		XCTAssert((literal("Hello") • ?<!literal("ell")).hasMatches(over: "Hello"))
+		XCTAssert((literal("Hello") • ?<!literal("ell")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testLeadingAssertion() {
+		XCTAssert((?<!literal("H") • literal("Hello")).hasMatches(over: "Hello"))
+		XCTAssert((?<!literal("H") • literal("Hello")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testMiddleAssertion() {
+		XCTAssert((literal("Hel") • ?<!literal("e") • literal("lo")).hasMatches(over: "Hello"))
+		XCTAssert((literal("Hel") • ?<!literal("e") • literal("lo")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testTrailingAssertion() {
+		XCTAssert((literal("Hello") • ?<!literal("l")).hasMatches(over: "Hello"))
+		XCTAssert((literal("Hello") • ?<!literal("l")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testNegatedTrailingAssertion() {
+		XCTAssert(!(literal("Hello") • ?<!literal("llo")).hasMatches(over: "Hello"))
+		XCTAssert(!(literal("Hello") • ?<!literal("llo")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
 }
--- Tests/PatternKitTests/Negated Forward Assertions Test Case.swift
+++ Tests/PatternKitTests/Negated Forward Assertions Test Case.swift
@@ -5,4 +5,34 @@ import XCTest
 
 class NegatedForwardAssertionsTestCase : XCTestCase {
 	
+	func testLeadingStringAssertion() {
+		XCTAssert((?!literal("ell") • literal("Hello")).hasMatches(over: "Hello"))
+		XCTAssert((?!literal("ell") • literal("Hello")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testMiddleStringAssertion() {
+		XCTAssert((literal("Hel") • ?!literal("e") • literal("lo")).hasMatches(over: "Hello"))
+		XCTAssert((literal("Hel") • ?!literal("e") • literal("lo")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testTrailingStringAssertion() {
+		XCTAssert((literal("Hello") • ?!literal("lo")).hasMatches(over: "Hello"))
+		XCTAssert((literal("Hello") • ?!literal("lo")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testLeadingEmptyAssertion() {
+		XCTAssert(!(?!literal("") • literal("Hello")).hasMatches(over: "Hello"))
+		XCTAssert(!(?!literal("") • literal("Hello")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testMiddleEmptyAssertion() {
+		XCTAssert(!(literal("Hel") • ?!literal("") • literal("lo")).hasMatches(over: "Hello"))
+		XCTAssert(!(literal("Hel") • ?!literal("") • literal("lo")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
+	func testTrailingEmptyAssertion() {
+		XCTAssert(!(literal("Hello") • ?!literal("")).hasMatches(over: "Hello"))
+		XCTAssert(!(literal("Hello") • ?!literal("")).hasMatches(over: "Hello", direction: .backward))
+	}
+	
 }
