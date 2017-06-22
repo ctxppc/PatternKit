f31d43016f6dc00072c895e742e4054a0a41d21c
Constantino Tsarouhas <ctxppc@gmail.com>
2017-06-22 18:35:38 +0200

    Made subpattern in Token mutable.

--- Sources/Token.swift
+++ Sources/Token.swift
@@ -1,43 +1,45 @@
 // PatternKit © 2017 Constantino Tsarouhas
 
-public final class Token<Subpattern : Pattern> where
-	Subpattern.ForwardMatchCollection.Iterator.Element == Match<Subpattern.Subject>,
-	Subpattern.BackwardMatchCollection.Iterator.Element == Match<Subpattern.Subject> {
+public final class Token<CapturedPattern : Pattern> where
+	CapturedPattern.ForwardMatchCollection.Iterator.Element == Match<CapturedPattern.Subject>,
+	CapturedPattern.BackwardMatchCollection.Iterator.Element == Match<CapturedPattern.Subject> {
+	
+	public typealias Subject = CapturedPattern.Subject
 	
 	/// Creates a token capturing matches from a subpattern.
 	///
 	/// - Parameter subpattern: The subpattern.
-	public init(_ subpattern: Subpattern) {
-		self.subpattern = subpattern
+	public init(_ subpattern: CapturedPattern) {
+		self.capturedPattern = subpattern
 	}
 	
-	/// The subpattern.
-	public let subpattern: Subpattern
+	/// The pattern whose matched subsequences are captured by the token.
+	public var capturedPattern: CapturedPattern
 	
 }
 
 extension Token : Pattern {
 	
-	public func forwardMatches(enteringFrom base: Match<Subpattern.Subject>) -> LazyMapBidirectionalCollection<Subpattern.ForwardMatchCollection, Match<Subpattern.Subject>> {
-		return subpattern
+	public func forwardMatches(enteringFrom base: Match<Subject>) -> LazyMapBidirectionalCollection<CapturedPattern.ForwardMatchCollection, Match<Subject>> {
+		return capturedPattern
 			.forwardMatches(enteringFrom: base)
 			.lazy
 			.map { $0.capturing(base.inputPosition..<$0.inputPosition, for: self) }
 	}
 	
-	public func backwardMatches(recedingFrom base: Match<Subpattern.Subject>) -> LazyMapBidirectionalCollection<Subpattern.BackwardMatchCollection, Match<Subpattern.Subject>> {
-		return subpattern
+	public func backwardMatches(recedingFrom base: Match<Subject>) -> LazyMapBidirectionalCollection<CapturedPattern.BackwardMatchCollection, Match<Subject>> {
+		return capturedPattern
 			.backwardMatches(recedingFrom: base)
 			.lazy
 			.map { $0.capturing($0.inputPosition..<base.inputPosition, for: self) }
 	}
 	
-	public func underestimatedSmallestInputPositionForForwardMatching(on subject: Subpattern.Subject, fromIndex inputPosition: Subpattern.Subject.Index) -> Subpattern.Subject.Index {
-		return subpattern.underestimatedSmallestInputPositionForForwardMatching(on: subject, fromIndex: inputPosition)
+	public func underestimatedSmallestInputPositionForForwardMatching(on subject: Subject, fromIndex inputPosition: Subject.Index) -> Subject.Index {
+		return capturedPattern.underestimatedSmallestInputPositionForForwardMatching(on: subject, fromIndex: inputPosition)
 	}
 	
-	public func overestimatedLargestInputPositionForBackwardMatching(on subject: Subpattern.Subject, fromIndex inputPosition: Subpattern.Subject.Index) -> Subpattern.Subject.Index {
-		return subpattern.overestimatedLargestInputPositionForBackwardMatching(on: subject, fromIndex: inputPosition)
+	public func overestimatedLargestInputPositionForBackwardMatching(on subject: Subject, fromIndex inputPosition: Subject.Index) -> Subject.Index {
+		return capturedPattern.overestimatedLargestInputPositionForBackwardMatching(on: subject, fromIndex: inputPosition)
 	}
 	
 }
