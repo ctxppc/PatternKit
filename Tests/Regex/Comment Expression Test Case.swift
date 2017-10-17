496f4365d6da57bd335902b0e22ba533af450909
Constantino Tsarouhas <ctxppc@gmail.com>
2017-10-17 18:16:46 +0200

    Added test case for Comment Expression.

---
+++ Tests/Regex/Comment Expression Test Case.swift
@@ -0,0 +1,18 @@
+// PatternKit © 2017 Constantino Tsarouhas
+
+import XCTest
+@testable import PatternKitRegex
+
+class CommentExpressionTestCase : XCTestCase {
+	
+	func testSimple() {
+		let expression = CommentRegularExpression("test string")
+		XCTAssert(try! expression.serialisation(language: .perlCompatibleREs) == "(?#test string)")
+	}
+	
+	func testNested() {
+		let expression = CommentRegularExpression("(?#test string")
+		XCTAssert(try! expression.serialisation(language: .perlCompatibleREs) == "(?#(?#test string)")
+	}
+	
+}
