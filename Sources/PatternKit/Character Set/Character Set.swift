0b6067f867d0c3ee0c844aa03d9fac04c0684fb6
Constantino Tsarouhas <c@tsarouhas.eu>
2021-11-30 20:38:06 +0100

    Changes Character Set's subject type to Substring which is more general than String: users can trivially trivially convert strings to substrings but vice versa requires a copy.

--- Sources/PatternKit/Character Set/Character Set.swift
+++ Sources/PatternKit/Character Set/Character Set.swift
@@ -5,25 +5,25 @@ import PatternKitCore
 
 extension CharacterSet : PatternKitCore.Pattern {
 	
-	public func forwardMatches(enteringFrom base: Match<String>) -> SingularMatchCollection<String> {
+	public func forwardMatches(enteringFrom base: Match<Substring>) -> SingularMatchCollection<Substring> {
 		
 		guard let character = base.remainingElements(direction: .forward).first else { return nil }
 		
 		let scalars = character.unicodeScalars
 		guard let scalar = scalars.first, scalars.count == 1, contains(scalar) else { return nil }
 		
-		return SingularMatchCollection(resultMatch: base.movingInputPosition(distance: 1, direction: .forward))
+		return .init(resultMatch: base.movingInputPosition(distance: 1, direction: .forward))
 		
 	}
 	
-	public func backwardMatches(recedingFrom base: Match<String>) -> SingularMatchCollection<String> {
+	public func backwardMatches(recedingFrom base: Match<Substring>) -> SingularMatchCollection<Substring> {
 		
 		guard let character = base.remainingElements(direction: .backward).last else { return nil }
 		
 		let scalars = character.unicodeScalars
 		guard let scalar = scalars.first, scalars.count == 1, contains(scalar) else { return nil }
 		
-		return SingularMatchCollection(resultMatch: base.movingInputPosition(distance: 1, direction: .backward))
+		return .init(resultMatch: base.movingInputPosition(distance: 1, direction: .backward))
 		
 	}
 	
